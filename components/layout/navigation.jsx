e3fc25e23915e3927b82624016e82d982773e637
Velizar Ganchev <velizar_ganchev@hotmail.com>
2023-11-09 13:22:43 +0100

    create project, add layout,navigatin,main,about,contact

---
+++ components/layout/navigation.jsx
@@ -0,0 +1,127 @@
+import { useState, useEffect } from 'react'
+
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai"
+import { FaGithub, FaLinkedinIn } from "react-icons/fa"
+import { BsFillPersonLinesFill } from "react-icons/bs"
+
+const Navigation = () => {
+
+    const [nav, setNav] = useState(false);
+    const [shadow, setShadow] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
+    useEffect(() => {
+        const handleShadow = () => {
+            if (window.scrollY >= 90) {
+                setShadow(true);
+            } else {
+                setShadow(false);
+            }
+        };
+        window.addEventListener('scroll', handleShadow)
+    }, []);
+    return (
+        <header className={shadow ? "fixed w-full shadow-xl h-20 z-[100]" : "fixed w-full h-20 z-[100] "}>
+            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+                <Link href="#">
+                    <Image src="/assets/logo.png" alt='Logo' width={100} height={100} />
+                </Link>
+                <nav>
+                    <ul className="hidden md:flex">
+                        <Link href="/">
+                            <li className="ml-10 text-sm uppercase hover:border-b border-emerald-200">Home</li>
+                        </Link>
+                        <Link href="/#about">
+                            <li className="ml-10 text-sm uppercase hover:border-b border-emerald-200">About</li>
+                        </Link>
+                        <Link href="/#skills">
+                            <li className="ml-10 text-sm uppercase hover:border-b border-emerald-200">Skills</li>
+                        </Link>
+                        <Link href="/#projects">
+                            <li className="ml-10 text-sm uppercase hover:border-b border-emerald-200">Projects</li>
+                        </Link>
+                        <Link href="/#contact">
+                            <li className="ml-10 text-sm uppercase hover:border-b border-emerald-200">Contact</li>
+                        </Link>
+                    </ul>
+                    <div
+                        onClick={handleNav}
+                        className="md:hidden cursor-pointer">
+                        <AiOutlineMenu size={30} />
+                    </div>
+                </nav>
+            </div>
+            <div className={nav ?
+                "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70"
+                : ""}>
+                <div className={nav
+                    ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500 bg-gradient-to-r from-emerald-100 to-emerald-900"
+                    : "fixed left-[-100%] top-0 p-10 ease-in duration-500"}>
+                    <div>
+                        <div className="flex w-full items-center justify-between">
+                            <Link href="/">
+                                <Image src="/assets/logo.png" alt="Navigation Logo" width={87} height={55}></Image>
+                            </Link>
+                            <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                                <AiOutlineClose />
+                            </div>
+                        </div>
+                        <div className="border-b border-gray-300 my-4">
+                            <p className="w-[85%] md:w-[90%] py-4">We can make your vehicle look like new again.</p>
+                        </div>
+                    </div>
+                    <div className="py-4 flex flex-col">
+                        <ul className="uppercase">
+                            <Link href="/">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">Home</li>
+                            </Link>
+                            <Link href="/#about">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">About</li>
+                            </Link>
+                            <Link href="/#skills">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">Skills</li>
+                            </Link>
+                            <Link href="/#projects">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">Projects</li>
+                            </Link>
+                            <Link href="/#contact">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">Contact</li>
+                            </Link>
+                        </ul>
+                        <div className="pt-40">
+                            <p className="uppercase tracking-widest text-[#549475]">Let&apos;s Connect</p>
+                            <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <a href="https://www.linkedin.com/in/velizar-ganchev-29311621b/">
+                                        <FaLinkedinIn />
+                                    </a>
+                                </div>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <a href="https://github.com/velizarganchev">
+                                        <FaGithub />
+                                    </a>
+                                </div>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <AiOutlineMail />
+                                </div>
+                                <Link href="/resume">
+                                    <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                        <BsFillPersonLinesFill />
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Navigation
