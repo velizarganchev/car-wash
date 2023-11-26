593c51c1fd3b1d696d460dc7fd1bc804a5534896
Velizar Ganchev <velizar_ganchev@hotmail.com>
2023-11-26 21:11:17 +0100

    add header and navigation

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,14 @@ import { Inter } from 'next/font/google'
 import Contact from '@/components/contact'
 import Main from '@/components/main'
 import About from '@/components/about'
+import Header from '@/components/header/header-component'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className=''>
-      <Main />
-      <About />
-      <Contact />
+    <main>
+      <Header />
     </main>
   )
 }
